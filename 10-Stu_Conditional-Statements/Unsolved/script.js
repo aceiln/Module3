26fc3f5007a0de3956306867885180dcad0ed28d
Ella <ellathitotran@gmail.com>
2023-06-22 04:44:47 -0500

    files and finished 10

--- 10-Stu_Conditional-Statements/Unsolved/script.js
+++ 10-Stu_Conditional-Statements/Unsolved/script.js
@@ -1,6 +1,37 @@
-// Change the values and operators below to test your algorithm meets all conditions
-var x = 50;
-var expression1 = (x < 25);
-var expression2 = (x > 50);
+var kiss = 16;
 
-// Write Your JavaScript Code Here
+//true true
+if(kiss<=15){
+    console.log('more kiss pls');
+} else {
+    console.log('i still want kiss');
+}
+//true false
+if (kiss < 10){
+    console.log('do u hate me or smth');
+}else if (kiss >= 10 && kiss <= 15) {
+    console.log('not enough kiss');
+}else {
+    console.log('mwah mwahmhwamhawmh')
+}
+//false true
+
+//false false
+
+
+
+
+
+
+
+/// var x = 50;
+/// var lower = (x < 25);
+/// var over = (x > 50);
+
+/// if(lower){
+///     console.log('less than 25');
+/// } else if(!over) {
+///     console.log('in between');
+/// }else {
+///     console.log('over 50');
+/// }
