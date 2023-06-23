d18dcff95151ac5bf4137d3cc51320396da03e53
Ella <ellathitotran@gmail.com>
2023-06-22 23:11:14 -0500

    day 3 done

--- 12-Stu_Arrays/Unsolved/script.js
+++ 12-Stu_Arrays/Unsolved/script.js
@@ -1,1 +1,18 @@
 // WRITE YOUR CODE HERE
+var andrews = ['Andrew', 'TheRoo', 'Nerd', 'Cutie'];
+
+console.log(andrews.length);
+console.log(andrews)
+console.log('welcome home ' + andrews[0]);
+console.log('welcome home ' + andrews[1]);
+console.log('welcome home ' + andrews[2]);
+console.log('welcome home ' + andrews[3]);
+
+var TheRoo = andrews[1];
+
+andrews[1]='My love'
+console.log(andrews);
+
+if(andrews[1] !== TheRoo) {
+    console.log('Found a better name; ' + andrews[1])
+}
